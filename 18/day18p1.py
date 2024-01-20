1315530071dd2a80c29fc0035a6814137b141326
gaplin <gaplinn@gmail.com>
2024-01-20 21:27:47 +0100

    18

---
+++ 18/day18p1.py
@@ -0,0 +1,33 @@
+first_row = [x for x in open('input2.txt').read().strip()]
+
+m = len(first_row)
+grid = [first_row]
+n = 1
+target_n = 40
+
+while n < target_n:
+    previous_row = grid[-1]
+    next_row = []
+    for i in range(m):
+        l, c, r = '.', '.', '.'
+        if i > 0:
+            l = previous_row[i - 1]
+        c = previous_row[i]
+        if i < m - 1:
+            r = previous_row[i + 1]
+        
+        if l == '^' and c == '^' and r != '^' or\
+            c == '^' and r == '^' and l != '^' or\
+            l == '^' and c != '^' and r != '^' or\
+            r == '^' and c != '^' and l != '^':
+            next_row.append('^')
+        else:
+            next_row.append('.')
+    grid.append(next_row)
+    n += 1
+
+result = 0
+for row in grid:
+    result += row.count('.')
+
+print(result)
